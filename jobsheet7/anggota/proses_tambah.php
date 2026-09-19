9bb74a57d2087575953898561f54a55a34b06a78
JVK50 <jidanali32@gmail.com>
2026-09-19 21:03:46 +0700

    Menambah beberapa validasi baru di proses tambah anggota serta flash massage

--- jobsheet7/anggota/proses_tambah.php
+++ jobsheet7/anggota/proses_tambah.php
@@ -10,8 +10,22 @@ $errors = [];
 if ($nama === '') {
     $errors[] = "Nama wajib diisi.";
 }
+
+// Penambahan format huruf/angka/tdhub
 if ($noAnggota === '') {
     $errors[] = "No. Anggota wajib diisi.";
+} elseif (!preg_match('/^[a-zA-Z0-9-]+$/', $noAnggota)) {
+    $errors[] = "No. Anggota hanya boleh berisi huruf, angka, dan tanda hubung (-).";
+}
+
+// Validasi No.Hp
+if ($noHp !== '' && !preg_match('/^[0-9\+\-\s]{8,15}$/', $noHp)) {
+    $errors[] = "No. HP harus berupa angka (8-15 digit).";
+}
+
+// Validasi Alamat (Maksimal 255 karakter)
+if (strlen($alamat) > 255) {
+    $errors[] = "Alamat tidak boleh lebih dari 255 karakter.";
 }
 
 if (!empty($errors)) {
